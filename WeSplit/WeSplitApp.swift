45d4a8279c14f0d59c4dcfe069b1190ae6689de1
ChaitanyaJJ <117849002+ChaitanyaJJ@users.noreply.github.com>
2025-04-10 21:02:31 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Chaitanya J on 10/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
